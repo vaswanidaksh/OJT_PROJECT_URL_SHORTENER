744cef72a799e7eadc215d13c5fda76686803b56
devanshbaghel18 <devanshbaghel059@gmail.com>
2025-12-04 03:00:40 +0530

    update

--- url_shortener_project/wsgi.py
+++ url_shortener_project/wsgi.py
@@ -1,12 +1,14 @@
 import os
 import sys
 
-path = '/home/devanshbaghel18/OJT_PROJECT_URL_SHORTENER'
-if path not in sys.path:
-    sys.path.append(path)
+project_path = '/home/devanshbaghel18/OJT_PROJECT_URL_SHORTENER'
+if project_path not in sys.path:
+    sys.path.append(project_path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shortner_project.settings')
+# Change directory to Django project folder
+os.chdir(os.path.join(project_path, 'url_shortener_project'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'url_shortener_project.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
