b12f9128e47da566963a1c8abc87173893c370d2
devanshbaghel18 <devanshbaghel059@gmail.com>
2025-12-04 02:53:08 +0530

    update

--- url_shortener_project/wsgi.py
+++ url_shortener_project/wsgi.py
@@ -1,21 +1,11 @@
 import os
 import sys
 
-# Path to your project
-project_home = '/home/ayoo/OJT_PROJECT_URL_SHORTENER'
+path = '/home/devanshbaghel18/OJT_PROJECT_URL_SHORTENER'
+if path not in sys.path:
+    sys.path.append(path)
 
-# Add project path to sys.path
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shortner_project.settings')
 
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'url_shortener_project.settings')
-
-# Activate virtualenv
-activate_this = '/home/ayoo/OJT_PROJECT_URL_SHORTENER/venv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
-# Get WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
