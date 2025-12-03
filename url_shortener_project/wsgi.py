232402b0e360420f36214a5d3e4743205fe23857
devanshbaghel18 <devanshbaghel059@gmail.com>
2025-12-04 02:17:12 +0530

    deploying in process

--- url_shortener_project/wsgi.py
+++ url_shortener_project/wsgi.py
@@ -1,16 +1,21 @@
-"""
-WSGI config for url_shortener_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Path to your project
+project_home = '/home/ayoo/OJT_PROJECT_URL_SHORTENER'
 
+# Add project path to sys.path
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'url_shortener_project.settings')
 
+# Activate virtualenv
+activate_this = '/home/ayoo/OJT_PROJECT_URL_SHORTENER/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Get WSGI application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
